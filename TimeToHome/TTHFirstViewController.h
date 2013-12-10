a34d977f2a53580eac77c3104842574745a82e48
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2013-12-10 00:05:18 -0800

    init

--- TimeToHome/TTHFirstViewController.h
+++ TimeToHome/TTHFirstViewController.h
@@ -7,7 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AFNetworking.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface TTHFirstViewController : UIViewController
+@interface TTHFirstViewController : UIViewController<CLLocationManagerDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *uberTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *busTimeLabel;
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
--- TimeToHome/TTHFirstViewController.m
+++ TimeToHome/TTHFirstViewController.m
@@ -20,6 +20,42 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+-(NSString *)getUberTimeToHome: (id)responseObject {
+    NSArray *routes = [responseObject objectForKey:@"routes"];
+    NSDictionary *route = [routes objectAtIndex:0];
+    NSArray *legs = [route objectForKey:@"legs"];
+    if (route) {
+        return [[[legs objectAtIndex:0] objectForKey:@"duration"] objectForKey:@"text"];
+    }
+    return @"Unable to contact the Goog";
+}
+
+- (void)updateUberWaitTimeTextLabel: (CLLocation *)location {
+    NSString *locationCoordinateString = [NSString stringWithFormat:@"%f,%f", location.coordinate.latitude, location.coordinate.longitude];
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET: @"http://maps.googleapis.com/maps/api/directions/json" parameters:@{@"origin": locationCoordinateString, @"destination": @"639 Geary Street, San Francisco CA, 94102", @"sensor": @"true"} success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSString *time = [self getUberTimeToHome:responseObject];
+        [_uberTimeLabel setText:time];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    if (nil == _locationManager)
+        _locationManager = [[CLLocationManager alloc] init];
+    
+    _locationManager.delegate = self;
+    [_locationManager startMonitoringSignificantLocationChanges];
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray *)locations {
+    CLLocation *location = [locations lastObject];
+    [self updateUberWaitTimeTextLabel:location];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
